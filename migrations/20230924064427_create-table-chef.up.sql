a39f435551fbdcad91cdbfd4aebbd2b7db9c849b
betawulandari <betawulandari21@gmail.com>
2023-09-24 13:45:51 +0700

    feat: add migrations

---
+++ migrations/20230924064427_create-table-chef.up.sql
@@ -0,0 +1,5 @@
+create table chef (
+id int auto_increment not null,
+name varchar(100) not null,
+primary key(id)
+);
---
+++ migrations/20230924064435_create-table-menu.up.sql
@@ -0,0 +1,5 @@
+create table menu (
+id int auto_increment not null,
+name varchar(100) not null,
+primary key(id)
+);
